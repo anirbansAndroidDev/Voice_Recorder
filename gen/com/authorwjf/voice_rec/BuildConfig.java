e2e0b2f3802c718e955cd6dbf4a4d0954b4951b8
Anirban Jana <anirbansjob@gmail.com>
2013-04-27 08:49:45 +0530

    Best optimized code
    Recording and play it

--- gen/com/authorwjf/voice_rec/BuildConfig.java
+++ gen/com/authorwjf/voice_rec/BuildConfig.java
@@ -1,5 +1,5 @@
 /** Automatically generated file. DO NOT MODIFY */
-package com.example.callrecorderproto1;
+package com.authorwjf.voice_rec;
 
 public final class BuildConfig {
     public final static boolean DEBUG = true;
